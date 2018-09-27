4d958e3abc01a4246de47dbd5d1d935480d36b93
DevDelvo <kevinbdelvo@gmail.com>
2018-09-27 00:32:24 -0400

    adjusted form to use flexbox with justify content. also added left padding

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,9 +3,13 @@ import React from 'react';
 const Form = (props) => {
 
     return (
-        <form onSubmit={props.getWeather}>
-            <span>Country: </span><input type="text" name="country" placeholder="Country Abbreviation" />
+        <form className="submit-form" onSubmit={props.getWeather}>
+            <div>
+                <span>Country: </span><input type="text" name="country" placeholder="Country Abbreviation" />
+            </div>
+            <div>
             <span> Zip code: </span><input type="text" name="zipcode" placeholder="Zip code" />
+            </div>
             <button>Get Weather</button>
         </form>
     )
